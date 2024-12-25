bc735d385b0676c9ff0153e1aac91b2abea8d164
Saurish <saurishsum10@outlook.com>
2024-12-25 03:13:07 -0800

    idk whats happening

--- src/app/api/Blogs/[slug]/route.ts
+++ src/app/api/Blogs/[slug]/route.ts
@@ -2,14 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
 
-type IParams = {
+type Props = {
   params: {
     slug: string;
   };
 };
 
 // If { params } looks confusing, check the note below this code block
-export async function GET(req: NextRequest, { params }: IParams) {
+export async function GET(req: NextRequest, { params }: Props) {
   await connectDB(); // function from db.ts before
   const { slug } = params; // another destructure
 
