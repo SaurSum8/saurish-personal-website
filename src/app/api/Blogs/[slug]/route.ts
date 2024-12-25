3409484bab60c5295ee0cc0a7dd7398c95819203
Saurish <saurishsum10@outlook.com>
2024-12-25 02:44:31 -0800

    attempt 6

--- src/app/api/Blogs/[slug]/route.ts
+++ src/app/api/Blogs/[slug]/route.ts
@@ -2,11 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
 
-type tParams = Promise<{ slug: string }>;
+type Params = { slug: string };
 
-export async function GET(req: NextRequest, context: { params: tParams }) {
-  await connectDB(); // function from db.ts before
-  const { slug } = await context.params; // another destructure
+export async function GET(req: NextRequest, { params }: { params: Params }) {
+  await connectDB();
+  const { slug } = params;
 
   try {
     const blog = await blogSchema.findOne({ slug }).orFail();
