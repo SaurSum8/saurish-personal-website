a0afdac2ddb22303a12fb022e6d65d807778a62d
Saurish <saurishsum10@outlook.com>
2024-12-25 00:38:29 -0800

    first commit

---
+++ src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import Comments from "@/components/comment";
+import BLOGType from "@/database/blogSchema";
+import CommentForm from "@/components/CommentForm";
+
+type Props = {
+  params: { slug: string };
+};
+
+async function getBlog(slug: string) {
+  try {
+    // This fetches the blog from an api endpoint that would GET the blog
+    const res = await fetch(`http://localhost:3000/api/Blogs/${slug}`, {
+      cache: "no-store",
+    });
+    // This checks that the GET request was successful
+    if (!res.ok) {
+      throw new Error("Failed to fetch blog");
+    }
+
+    return res.json();
+  } catch (err: unknown) {
+    console.log(`error: ${err}`);
+    return null;
+  }
+}
+
+export default async function Blog({ params: { slug } }: Props) {
+  const cmnt: BLOGType = await getBlog(slug);
+  const comList = cmnt["comments"];
+  comList.sort((a, b) => a.time.localeCompare(b.time));
+
+  return (
+    <div className={"blog-element"}>
+      <h1>{cmnt["title"]}</h1>
+      <div className={"blog-img"}>
+        <Image
+          src={cmnt["image"]}
+          alt={cmnt["image_alt"]}
+          width={300}
+          height={300}
+        ></Image>
+      </div>
+      {cmnt["content"]}
+      <h2>Comments</h2>
+      {comList.map((fcomment, index) => (
+        <Comments
+          user={fcomment.user}
+          comment={fcomment.comment}
+          time={fcomment.time}
+          key={index}
+        />
+      ))}
+      <div className="blog-element">
+        <h5>Add Your Comment Here!</h5>
+        <CommentForm slug={slug} />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function footer() {
+  return (
+    <footer className="footer">
+      © 2025 Saurish's Website | All Rights Reserved
+    </footer>
+  );
+}
