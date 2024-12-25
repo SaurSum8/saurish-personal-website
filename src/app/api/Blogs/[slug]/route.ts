3b2578fad47e251d3d429d7ab63d7131b10fda94
Saurish <saurishsum10@outlook.com>
2024-12-25 03:03:51 -0800

    attempt 8

--- src/app/api/Blogs/[slug]/route.ts
+++ src/app/api/Blogs/[slug]/route.ts
@@ -2,11 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
 
-export async function GET(req: NextRequest, slug: string) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { slug: string } }
+) {
   await connectDB();
 
   try {
-    const blog = await blogSchema.findOne({ slug }).orFail();
+    const blog = await blogSchema.findOne({ slug: params.slug }).orFail();
     return NextResponse.json(blog);
   } catch (err) {
     console.log("GET API ERROR: ", err);
