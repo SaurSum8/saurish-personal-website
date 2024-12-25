d80859663951b0b336764c93e4a15edc408fcaeb
Saurish <saurishsum10@outlook.com>
2024-12-25 03:09:09 -0800

    original state

--- src/app/api/Blogs/[slug]/route.ts
+++ src/app/api/Blogs/[slug]/route.ts
@@ -2,15 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
 
-export async function GET(req: NextRequest) {
-  await connectDB();
-  const slug = req.body;
+type IParams = {
+  params: {
+    slug: string;
+  };
+};
+
+// If { params } looks confusing, check the note below this code block
+export async function GET(req: NextRequest, { params }: IParams) {
+  await connectDB(); // function from db.ts before
+  const { slug } = params; // another destructure
 
   try {
     const blog = await blogSchema.findOne({ slug }).orFail();
     return NextResponse.json(blog);
   } catch (err) {
-    console.log("GET API ERROR: ", err);
     return NextResponse.json("Blog not found.", { status: 404 });
   }
 }
