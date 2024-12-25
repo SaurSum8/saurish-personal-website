031f9aaeceacb75a19ec075d28ba76c591fc35f6
Saurish <saurishsum10@outlook.com>
2024-12-25 02:37:08 -0800

    attempt 5 at fixing GET build issue

--- src/app/api/Blogs/[slug]/route.ts
+++ src/app/api/Blogs/[slug]/route.ts
@@ -2,15 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
 
-interface RouteParams {
-  params: {
-    slug: string;
-  };
-}
+type tParams = Promise<{ slug: string }>;
 
-export async function GET(req: NextRequest, context: RouteParams) {
+export async function GET(req: NextRequest, context: { params: tParams }) {
   await connectDB(); // function from db.ts before
-  const { slug } = context.params; // another destructure
+  const { slug } = await context.params; // another destructure
 
   try {
     const blog = await blogSchema.findOne({ slug }).orFail();
